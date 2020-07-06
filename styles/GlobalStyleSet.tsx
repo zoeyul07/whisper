7f1c6a3c012d3acb18f1f981e9b3312c280601e5
junyeongCHOI <asxd153@gmail.com>
2020-07-06 20:03:56 +0900

    project init

---
+++ styles/GlobalStyleSet.tsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyleSet = createGlobalStyle`
+    ${reset}
+`;
+
+export default GlobalStyleSet;
