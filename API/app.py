3e2655c34fccf6774463d48aa7077ceb17c0cd7a
2cong <cjina0418@gmail.com>
2020-07-08 16:43:37 +0900

    Add: series 생성 api

--- API/app.py
+++ API/app.py
@@ -3,11 +3,12 @@ from flask_cors import CORS
 
 from controllers.diary import diary_app
 from controllers.series import series_app
+from controllers.user import user_app
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.register_blueprint(diary_app, url_prefix='/diary')
     app.register_blueprint(series_app, url_prefix='/series')
-    app.register_blueprint('user', url_prefix='/user')
+    app.register_blueprint(user_app, url_prefix='/user')
     return app
--- API/controllers/series.py
+++ API/controllers/series.py
@@ -1,9 +1,44 @@
 import os, sys
 
 BASE_DIR = os.path.dirname(os.path.abspath("API"))
-sys.path.exten([BASE_DIR])
+sys.path.extend([BASE_DIR])
 
 import pymysql
-import flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify
+
+from connections import db_connector
+from models import ModelDao
 
 series_app = Blueprint("series_app", __name__)
+model_dao = ModelDao()
+
+@series_app.route('', methods=['POST'])
+def new_series():
+    """새로운 시리즈 생성 API
+
+    """
+
+    db = None
+    try:
+        # user_id = 토큰에서 받아온 user_id
+        user_id = 1
+        name = request.json['name']
+
+        db = db_connector()
+        # db 에러처리
+        if db is None:
+            return jsonify(message="DATABASE_INIT_ERROR"), 500
+
+        # db에 값 추가
+        db.begin()
+        model_dao.insert_series(db, user_id, name)
+        db.commit()
+
+        return (''), 200
+
+    except Exception as e:
+        db.rollback()
+        return jsonify(message=f"{e}"), 500
+    finally:
+        if db:
+            db.close()
