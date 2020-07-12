b7dbb9a6c03c9194e70c20c6efa1071a999263f0
Soncl97 <chaelinson832@gmail.com>
2020-07-12 20:46:05 +0900

    bug fix

---
+++ UI/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div>
+      <div>email</div>
+      <div>kakao</div>
+      <div>google</div>
+    </div>
+  );
+}
+
+export default SignUp;
