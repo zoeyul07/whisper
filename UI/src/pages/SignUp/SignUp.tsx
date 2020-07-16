63b7db0f5f382ce52953fa1b66e9f0fd80247a60
Soncl97 <chaelinson832@gmail.com>
2020-07-16 14:32:08 +0900

    routes

--- UI/src/pages/SignUp/SignUp.tsx
+++ UI/src/pages/SignUp/SignUp.tsx
@@ -1,13 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Kakao from "kakaojs";
 
 function SignUp() {
-  return (
+  const kakaoLogin = async () => {
+    Kakao.Auth.login({
+      success: function (res: object) {
+        try {
+          const response = await axios.post(`진아님 api`, {
+            nickname: "nickname",
+          })
+          if(response.status === 200) {
+            console.log(response);
+          }
+        } catch (e) {
+          console.log(e.response);
+        }
+            if (res.message === "회원가입 필요") {
+              return (
+                console.log("info.id: ", res.info.id),
+                props.socialId(res.info.id),
+                props.history.push("/social/signUp")
+              );
+            } else {
+              return (
+                localStorage.setItem("token", res.token),
+                props.history.push("/")
+              );
+            });
+      },
+      fail: function (err) {
+        console.log(err);
+        notify(err);
+      },
+    });
+  };
+
+  return(
     <div>
-      <div>email</div>
-      <div>kakao</div>
-      <div>google</div>
+      <Link to="signUp/Email">
+        <button>이메일</button>
+      </Link>
+      <button>카카오</button>
+      <button>구글</button>
     </div>
-  );
+  )
 }
 
 export default SignUp;
