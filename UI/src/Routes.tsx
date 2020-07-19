1b23d7d52c17c3cb52e49336bbb91244b5f56e18
Soncl97 <chaelinson832@gmail.com>
2020-07-19 17:05:39 +0900

    mk signIn and modified signUp

--- UI/src/Routes.tsx
+++ UI/src/Routes.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignIn from "./pages/signIn/SignIn";
 import SignUp from "./pages/signUp/SignUp";
-import EmailSignUp from "./pages/signUp/EmailSignUp";
 
 function Routes() {
   return (
@@ -10,7 +9,6 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={SignIn}/>
         <Route exact path="/signUp" component={SignUp}/>
-        <Route exact path="/signUp/email" component={EmailSignUp}/>
       </Switch>
     </Router>
   );
